6bdd057358bfda5af513f433de6b0ddab96a8934
Rez <rhythmupa@gmail.com>
2025-10-05 13:32:17 +0530

    rename file and update file to prep for next work

--- src/lib/characters/charactersMain.svelte
+++ src/lib/characters/charactersMain.svelte
@@ -1,4 +1,4 @@
-<div class="flex h-3/10 border-2 border-red-500">
+<div class="flex h-4/10 border-2 border-red-500">
 	<div class="h-full w-1/2 border-2 border-red-500"></div>
 	<div class="h-full w-1/2 border-2 border-red-500"></div>
 </div>
--- src/lib/logic.js
+++ src/lib/logic.js
@@ -1,11 +1,5 @@
 import { addNewBubble } from './speech/speechMain.svelte';
 
 export function Next() {
-	const options = [
-		{ sayer: 'god', text: 'Hello there, I am God.' },
-		{ sayer: 'player', text: 'I am a player.' },
-		{ sayer: 'narrator', text: 'iim a Narrator' }
-	];
-	const choice = options[Math.floor(Math.random() * options.length)];
 	addNewBubble(choice.text, choice.sayer);
 }
