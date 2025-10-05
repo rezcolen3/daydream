4d3d7349ad8f11f070e4cd221a0d0a6bef8f4029
Rez <rhythmupa@gmail.com>
2025-10-05 09:35:24 +0530

    eee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,13 @@
 	import God from './speach/god.svelte';
 	import Narator from './speach/narator.svelte';
 	import Player from './speach/player.svelte';
+
+	import {speaches} from "./script"
 </script>
 
 <div class="flex h-full flex-col border-2 border-red-500">
 	<div class="flex h-3/10 flex-col gap-2 overflow-auto border-2 border-red-500 p-2">
+		
 		<Narator text="You wake up, you see the broke life you are living in" />
 	</div>
 	<div class="flex h-5/10 border-2 border-red-500">
---
+++ src/routes/script.js
@@ -0,0 +1,7 @@
+export let speaches = [
+	{ who: 'god', text: 'Hello there, I am God.' },
+	{ who: 'player', text: 'Hello God, I am your creation.' },
+	{ who: 'narator', text: 'And so, the story begins...' }
+];
+
+export function addNewSpeach(text, who) {}
