37ad98a2fc01fc5b7c036808ce5e74d4211c1e96
Rez <rhythmupa@gmail.com>
2025-10-05 16:17:26 +0530

    eee fix img

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 
 <div
 	class="flex h-full flex-col"
-	style="background-image: url('/src/lib/assets/bg.png'); background-size: cover; background-position: center;"
+	style="background-image: url('https://hc-cdn.hel1.your-objectstorage.com/s/v3/e4789903a035a7bb7bd091ba9ac46728b7e0832c_image.png'); background-size: cover; background-position: center;"
 >
 	{#if true == true}
 		<!-- simulating game started -->
